a5e7c5eb9317d09637a3a959472bac668bf37df4
infobarbosa <infobarbosa@yahoo.com.br>
2023-11-19 17:44:56 -0300

    incluindo env vars influxdb no script

--- scripts/pedidos.sh
+++ scripts/pedidos.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+
+export INFLUX_CONFIG_NAME=default
+export INFLUX_BUCKET_NAME=default
+export INFLUX_HOST=http://localhost:8086
+export INFLUX_ORG=infobarbosa
+export INFLUX_TOKEN=3y1c3NnlmA1kA061YlROSO0gE5a1ppH_1Ig5HSMCsCX3VKF6zkrBwAtC-Hr6c_TTU8B9kwYOPphDq6hwyw5tLw==
+export INFLUX_USER=barbosa
+export INFLUX_PASSWORD=pessimasenha
+
 produtos=("TV" "GELADEIRA" "TV" "HOMETHEATER" "COMPUTADOR" "MONITOR" "TABLET" "SOUNDBAR" "CELULAR" "NOTEBOOK")
 for i in `seq 10000`
 do

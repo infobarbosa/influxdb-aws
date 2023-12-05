f5c69a5d3281130884245ceab8ab35b36f4d935f
infobarbosa <infobarbosa@yahoo.com.br>
2023-12-05 14:33:52 -0300

    intervalo de 100ms + loop maior

--- scripts/pedidos1.sh
+++ scripts/pedidos1.sh
@@ -9,7 +9,7 @@ export INFLUX_USER=barbosa
 export INFLUX_PASSWORD=pessimasenha
 
 produtos=("TV" "GELADEIRA" "TV" "HOMETHEATER" "COMPUTADOR" "MONITOR" "TABLET" "SOUNDBAR" "CELULAR" "NOTEBOOK")
-for i in `seq 10000`
+for i in `seq 1000000`
 do
     if [ $(expr $i % 2 ) != "0" ]; then
         influx write --bucket default "pedidos,produto=${produtos[${RANDOM:0:1}]},pais=BR Quantity=``${RANDOM:0:1}`` `date +%s%N`"
@@ -17,5 +17,5 @@ do
         influx write --bucket default "pedidos,produto=${produtos[${RANDOM:0:1}]},pais=US Quantity=``${RANDOM:0:1}`` `date +%s%N`"
     fi
 
-    sleep 0.05
+    sleep 0.1
 done
